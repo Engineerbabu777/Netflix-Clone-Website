f200fcdc393d01297f5cb01b27cfc422880f1beb
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-11-16 21:16:00 +0500

    'some-more-code'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,49 @@
+import React from 'react';
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
 
-export default function Home() {
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import { data } from '@/data';
+// import InfoModal from '@/components/InfoModal';
+// import useMovieList from '@/hooks/useMovieList';
+// import useFavorites from '@/hooks/useFavorites';
+// import useInfoModalStore from '@/hooks/useInfoModalStore';
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
+const Home = () => {
+  // const { data: movies = [] } = useMovieList();
+  // const { data: favorites = [] } = useFavorites();
+  // const {isOpen, closeModal} = useInfoModalStore();
+
   return (
     <>
-    <h1>Netflix Clone</h1>
+      {/* <InfoModal visible={isOpen} onClose={closeModal} /> */}
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={data} />
+        <MovieList title="My List" data={data} />
+      </div>
     </>
   )
 }
+
+export default Home;
